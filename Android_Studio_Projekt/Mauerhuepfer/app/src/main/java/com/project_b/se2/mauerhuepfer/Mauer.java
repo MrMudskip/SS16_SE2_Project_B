b3ace602e063ce2b377e3c65dd308906fe9ee166
anroses <croanni@gmail.com>
2016-05-02 19:53:08 +0200

    spieler
    noch nicht fertig

--- Android_Studio_Projekt/Mauerhuepfer/app/src/main/java/com/project_b/se2/mauerhuepfer/Mauer.java
+++ Android_Studio_Projekt/Mauerhuepfer/app/src/main/java/com/project_b/se2/mauerhuepfer/Mauer.java
@@ -4,42 +4,58 @@ package com.project_b.se2.mauerhuepfer;
  * Created by Anita on 02.05.2016.
  */
 public class Mauer {
-    int position1;
-    int position2;
-    int[] mauer1_3 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-    int[] mauer2_4 = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int p;     //position alt
+    int p2;     //position neu
+    int wuerfel;
+    int[] mauer1 = {1, 5, 6, 5, 4, 3, 2, 1};
+    int[] mauer2 = {1, 2, 3, 4, 5, 6, 5, 4};
+    int[] mauer3 = {1, 5, 6, 5, 4, 3, 2, 1};
+    int[] mauer4 = {1, 2, 3, 4, 5, 6, 5, 4};
+    int[] mauer5 = {5, 6, 5, 4, 3, 2, 1};
 
-    public int wuerfeln(){
-        return (int)(Math.random() * 6 + 1);
+    public Mauer (int p, int wuerfel){
+        this.p=p;
+        this.wuerfel=wuerfel;
     }
 
     public void mauer1(){
 
-
-        if (mauer1_3.length==wuerfeln()){
-            position1=position2;
+        if (mauer1.length==wuerfel){
+            switch (mauer1.length){
+                case 1:
+                    p=p+18; break;
+                case 5:
+            }
         }
-
     }
 
     public void mauer2(){
-        if (mauer2_4.length==wuerfeln()){
-            position1=position2;
+
+        if (mauer2.length==wuerfel){
+            p=p2;
         }
     }
 
     public void mauer3(){
-        if (mauer1_3.length==wuerfeln()){
-            position1=position2;
+
+        if (mauer3.length==wuerfel){
+            p=p2;
         }
     }
 
     public void mauer4(){
-        if (mauer2_4.length==wuerfeln()){
-            position1=position2;
+
+        if (mauer4.length==wuerfel{
+            p=p2;
         }
     }
 
+    public void mauer5(){
+
+        if (mauer5.length==wuerfel){
+            p=p2;
+        }
+    }
 
 
 
---
+++ Android_Studio_Projekt/Mauerhuepfer/app/src/main/java/com/project_b/se2/mauerhuepfer/Spieler.java
@@ -0,0 +1,55 @@
+package com.project_b.se2.mauerhuepfer;
+
+import java.util.Vector;
+
+/**
+ * Created by Anita on 02.05.2016.
+ */
+public class Spieler {
+    private String name;                //Name des Spielers
+    private int position;
+    private int[] spielfeld = {58};
+    private int anzahl;                 //Anzahl der Kegel
+    private int wuerfel;
+
+    public Spieler(String name){
+        this.name=name;
+        this.position=-1;
+    }
+
+    public void bewege(int newposition){
+        this.position=newposition;
+    }
+
+    public void entferne(){
+        this.position=-1;
+    }
+
+    public void mauer(){
+        Mauer m = new Mauer(position, wuerfel);
+        
+        while (spielfeld.length<=7){
+            m.mauer1();
+        }
+        while (spielfeld.length>9 || spielfeld.length<=17){
+            m.mauer2();
+        }
+        while (spielfeld.length>19 || spielfeld.length<=27){
+            m.mauer3();
+        }
+        while (spielfeld.length>29 || spielfeld.length<=37){
+            m.mauer4();
+        }
+        while (spielfeld.length>40 || spielfeld.length<=47){
+            m.mauer5();
+        }
+        if (spielfeld.length==40){
+            position=spielfeld[58];
+        }
+    }
+
+
+
+
+
+}
