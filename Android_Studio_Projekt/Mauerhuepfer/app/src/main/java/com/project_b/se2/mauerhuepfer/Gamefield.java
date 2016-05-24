016fb8812e7aff1fe1ff2e56bfc8e5e161531a41
Julian Janda <jjanda@edu.aau.at>
2016-05-24 16:26:00 +0200

    Modified Gamefield class
    Recovery 2/2

--- Android_Studio_Projekt/Mauerhuepfer/app/src/main/java/com/project_b/se2/mauerhuepfer/Gamefield.java
+++ Android_Studio_Projekt/Mauerhuepfer/app/src/main/java/com/project_b/se2/mauerhuepfer/Gamefield.java
@@ -1,11 +1,34 @@
 package com.project_b.se2.mauerhuepfer;
 
-import java.util.Vector;
-
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ImageView;
 /**
  * Created by Anita on 03.05.2016.
  */
 public class Gamefield {
+
+    // Block types
+    static final int S = 0;
+    static final int H = 1;
+    static final int E = 2;
+    static final int V = 3;
+    static final int F = 4;
+    static final int W = 5;
+    static final int N = 6;
+    static final int BR = 7;
+    static final int BG = 8;
+    static final int BY = 9;
+    static final int BB = 10;
+    static final int GR = 11;
+    static final int GG = 12;
+    static final int GY = 13;
+    static final int GB = 14;
+
+    // Other variables
     private int dice1;
     private int dice2;
     private Spieler player;
@@ -15,10 +38,45 @@ public class Gamefield {
     private Block field;
     private Block fin;
 
+    // 2D array containing all the blocks that make up the game field.
+    // TODO initiate all blocks of the game field.
+    private Block[][] gameField = {
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+            {new Block(H),new Block(H),new Block(H),new Block(H)},
+    };
 
     public Gamefield(Spieler player) {
         this.player=player;
         this.position=-1;
+    }
+
+    public void setBlockParametersByType(Block block, int type) {
+        switch (type) {
+            case S: {
+                // TODO handle this type.
+            }
+            case H: {
+                Resources res = mContext.getResources(); // TODO execute this on some higher level (only once).
+                Drawable img = res.getDrawable(R.drawable.w1n); // TODO Replace placeholder image
+                block.setImage(img);
+            }
+            // TODO handle the other block types.
+        }
+    }
+
+    public void drawGameField(){
+
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+
+            }
+        }
     }
 
     public void moveFigure(){
